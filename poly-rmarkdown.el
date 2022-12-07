5667ebe03e580c02666aa492ea726e8c50e7b970
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-12-06 19:32:33 -0600

    add exporters

--- poly-rmarkdown.el
+++ poly-rmarkdown.el
@@ -28,6 +28,97 @@
   :hostmode 'poly-rmarkdown-hostmode
   :innermodes '(poly-rmarkdown-innermode))
 
+;; export
+(defun pm--rmarkdown-output-file-sniffer ()
+  (goto-char (point-min))
+  (let (files)
+    (while (re-search-forward "Output created: +\\(.*\\)" nil t)
+      (push (expand-file-name (match-string 1)) files))
+    (reverse (delete-dups files))))
+
+(defun pm--rmarkdown-output-file-from-.Last.value ()
+  (ess-get-words-from-vector "print(.Last.value)\n"))
+
+(defun pm--rmarkdown-shell-auto-selector (action &rest _ignore)
+  (cl-case action
+    (doc "AUTO DETECT")
+    (command "Rscript -e \"rmarkdown::render('%i', output_format = 'all')\"")
+    (output-file #'pm--rmarkdown-output-file-sniffer)))
+
+(defun pm--rmarkdown-shell-default-selector (action &rest _ignore)
+  (cl-case action
+    (doc "DEFAULT")
+    (command "Rscript -e \"rmarkdown::render('%i', output_format = NULL)\"")
+    (output-file #'pm--rmarkdown-output-file-sniffer)))
+
+(defcustom poly-r-markdown-exporter
+  (pm-shell-exporter :name "Rmarkdown"
+                     :from
+                     '(("Rmarkdown"  "\\.[rR]?md\\|rapport\\'" "R Markdown"
+                        "Rscript -e \"rmarkdown::render('%i', output_format = '%t', output_file = '%o')\""))
+                     :to
+                     '(("auto" . pm--rmarkdown-shell-auto-selector)
+                       ("default" . pm--rmarkdown-shell-default-selector)
+                       ("html" "html" "html document" "html_document")
+                       ("pdf" "pdf" "pdf document" "pdf_document")
+                       ("word" "docx" "word document" "word_document")
+                       ("md" "md" "md document" "md_document")
+                       ("ioslides" "html" "ioslides presentation" "ioslides_presentation")
+                       ("slidy" "html" "slidy presentation" "slidy_presentation")
+                       ("beamer" "pdf" "beamer presentation" "beamer_presentation")))
+  "R Markdown exporter.
+  Please not that with 'AUTO DETECT' export options, output file
+  names are inferred by Rmarkdown from YAML description
+  block. Thus, output file names don't comply with
+  `polymode-exporter-output-file-format'."
+  :group 'polymode-export
+  :type 'object)
+
+(defun pm--rmarkdown-callback-auto-selector (action &rest _ignore)
+  (cl-case action
+    (doc "AUTO DETECT")
+    ;; last file is not auto-detected unless we cat new line
+    (command "rmarkdown::render('%I', output_format = 'all', knit_root_dir=getwd())")
+    (output-file #'pm--rmarkdown-output-file-from-.Last.value)))
+
+(defun pm--rmarkdown-callback-default-selector (action &rest _ignore)
+  (cl-case action
+    (doc "DEFAULT")
+    ;; last file is not auto-detected unless we cat new line
+    (command "rmarkdown::render('%I', output_format = NULL, knit_root_dir=getwd())")
+    (output-file #'pm--rmarkdown-output-file-from-.Last.value)))
+
+
+(defcustom poly-r-markdown-ess-exporter
+  (pm-callback-exporter :name "Rmarkdown-ESS"
+                        :from
+                        '(("Rmarkdown" "\\.[rR]?md\\|rapport\\'" "R Markdown"
+                           "rmarkdown::render('%I', output_format = '%t', output_file = '%O', knit_root_dir=getwd())\n"))
+                        :to
+                        '(("auto" . pm--rmarkdown-callback-auto-selector)
+                          ("html" "html" "html document" "html_document")
+                          ("pdf" "pdf" "pdf document" "pdf_document")
+                          ("default" . pm--rmarkdown-callback-default-selector)
+                          ("word" "docx" "word document" "word_document")
+                          ("md" "md" "md document" "md_document")
+                          ("ioslides" "html" "ioslides presentation" "ioslides_presentation")
+                          ("slidy" "html" "slidy presentation" "slidy_presentation")
+                          ("beamer" "pdf" "beamer presentation" "beamer_presentation"))
+                        :function 'pm--ess-run-command
+                        :callback 'pm--ess-callback)
+  "R Markdown exporter.
+  Please not that with 'AUTO DETECT' export options, output file
+  names are inferred by Rmarkdown from YAML description
+  block. Thus, output file names don't comply with
+  `polymode-exporter-output-file-format'."
+  :group 'polymode-export
+  :type 'object)
+
+(polymode-register-exporter poly-r-markdown-ess-exporter nil
+                            poly-markdown+r-polymode)
+(polymode-register-exporter poly-r-markdown-exporter nil
+                            poly-markdown+r-polymode)
+
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.[rR]md\\'" . poly-rmarkdown-mode))
 
